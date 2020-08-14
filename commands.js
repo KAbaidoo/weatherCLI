619857e577c92a8b4453431503bd4c384b0bbf82
KAbaidoo <KAbaidoo@hotmail.com>
2020-08-14 14:23:03 +0000

    first commit

---
+++ commands.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch');
+const API_KEY = "a8e19a67de0d4680a6dd1d1db20b4408";
+
+// fetch current weather condition for specified city
+const current = async (city) => {
+    try {
+        let response = await fetch(
+            `http://api.weatherstack.com/current?access_key=${API_KEY}&query=${city}`
+        );
+        let data = await response.json();
+        console.log(data);
+    } catch (e) {
+        console.log("Error : ", e.message);
+    }
+};
+
+const forcast = async (city) => {
+    try {
+        let response = await fetch(
+            `http://api.weatherstack.com/forecast?access_key=${API_KEY}&query=${city}`
+        );
+        let data = await response.json();
+        console.log(data);
+    } catch (e) {
+        console.log("Error : ", e.message);
+    }
+};
+
+module.exports = {
+    current,
+    forcast
+};
---
+++ index.js
@@ -0,0 +1,16 @@
+const program = require("commander");
+const { current, forcast } = require("./commands");
+
+program.version("0.0.1").description("A command line weather application");
+
+program
+  .command("current <city>")
+  .alias("c")
+  .description("Get current weather for specified city")
+  .action((city) => current(city));
+
+program
+  .command("current <city>")
+  .alias("f")
+  .description("Get weather forcast for specified city")
+  .action((city) => forcast(city));
